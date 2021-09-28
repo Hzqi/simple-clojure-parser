5a2f24ce2810302512a1839d363eda6c0232fdec
Hzqi <Hzqi_110@hotmail.com>
2021-09-28 17:21:09 +0800

    add lib function in core

--- src/simple_parser/core.clj
+++ src/simple_parser/core.clj
@@ -1,6 +1,8 @@
-(ns simple-parser.core)
+(ns simple-parser.core
+  (:require [simple-parser.parser :as p]
+            [simple-parser.result :as r]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn run-parser [parser input]
+  (let [res (p/exec parser [input 0])]
+    (r/print-error res)
+    res))
